54028eee873417bdff78d7775c1cb4cee011fd08
MRINFINITY99 <136961002+MRINFINITY99@users.noreply.github.com>
2023-06-18 16:59:34 +0530

    Update subsetsums.cpp

--- subsetsums.cpp
+++ subsetsums.cpp
@@ -5,6 +5,7 @@ class Solution{
     void func(int ind,int sum,vector<int>&arr,int N,vector<int>&sumSubset){
         if(ind==N){
             sumSubset.push_back(sum);
+            return;
         }
         func(ind+1,sum+arr[ind],arr,N,sumSubset);
         func(ind+1,sum,arr,N,sumSubset);
